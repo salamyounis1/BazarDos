5a5201d39314943681421c6faeffee6553e62e36
salamyounis1 <s11923533@stu.najah.edu>
2023-10-30 18:13:16 +0200

    Frontservice

---
+++ Documents/NetBeansProjects/mavenproject1/src/main/java/FrontendService.java
@@ -0,0 +1,22 @@
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author twitter
+ */
+
+import static spark.Spark.*;
+import org.json.JSONObject;
+
+public class FrontendService {
+    public static void main(String[] args) {
+     
+       
+    }
+}
