f61b9804d3e6336ab0d8402e7e29ccb378f59e70
RoaGQino <roaqino@gmail.com>
2023-10-31 14:27:29 +0300

    Connect to database sqlite

--- src/main/java/order.java
+++ src/main/java/order.java
@@ -8,19 +8,37 @@
  * @author ROA'A QINO
  */
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import static spark.Spark.*;
 import org.json.JSONObject;
 
 public class order {
+    private static Connection conData; // Define the connection as a class-level instance variable
+
     public static void main(String[] args) {
         port(8085);
-            
+
+        conData = connectToDatabase(); // Assign the connection
+
         get("/purchase/:ID", (req, res) -> {
-            String NumItem = req.params(":ID");
-            return purchase(NumItem);
+            String itemNumber = req.params(":ID");
+            return purchase(itemNumber);
         });
     }
     public static String purchase(String itemNumber) {
         return "";
+    }
+    public static Connection connectToDatabase() {
+        Connection conn = null;
+        try {
+            Class.forName("org.sqlite.JDBC");
+            conn = DriverManager.getConnection("jdbc:sqlite:sqliteDB.db");
+            System.out.println("Database connection established.");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        return conn;
     }
 }
