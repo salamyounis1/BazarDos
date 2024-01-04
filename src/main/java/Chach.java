aff2a601abae8893fe0bac989c779a796efe38f2
RoaGQino <roaqino@gmail.com>
2024-01-04 10:03:26 -0800

    ADD method to chach class

--- src/main/java/Chach.java
+++ src/main/java/Chach.java
@@ -3,5 +3,15 @@ import java.util.concurrent.ConcurrentHashMap;
 
 class Cache {
     private static final ConcurrentHashMap<String, String> cache_Memory = new ConcurrentHashMap<>();
+ public static String get(String item) {
+        return cache_Memory.get(item);
+    }
 
+    public static void put(String item, String value) {
+        cache_Memory.put(item, value);
+    }
+
+    public static void invalidate(String item) {
+        cache_Memory.remove(item);
+    }
 }
